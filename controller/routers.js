b18baa91d13c73b064227df67c395a960e111665
xinyong4 <1835346826@qq.com>
2017-08-21 16:09:16 +0800

    position

--- controller/routers.js
+++ controller/routers.js
@@ -2,5 +2,5 @@ module.exports = function(app){
     console.log("routers");
     app.use(require('./user'));
     app.use(require('./signin'));
-    app.use(require('./resume'));
+    app.use(require('./resume'));  app.use(require('./position.js'));
 }
---
+++ service/positionService.js
@@ -0,0 +1,32 @@
+const Position=require('../model/position');
+
+exports.getPosition=function(query){
+    var position=Position.findOne(query);
+    console.log('get position:'+position);
+    return position;
+}
+
+exports.getPositions=function(query){
+    return Position.find(query).exec();
+}
+
+exports.deletePosition=function(positionId){
+    console.log("delete position id:"+positionId);
+    return Position.remove({positionId:positionId});
+}
+
+exports.savePosition=function(position){
+    console.log("save position:"+JSON.stringify(position));
+    try{
+        var position=new Position(position).save();
+        return position;
+    }catch(err){
+        console.log('err:'+err);
+        return null;
+    }
+}
+
+exports.updatePosition=function(position){
+    console.log("update position:"+JSON.stringify(position));
+    return Position.where({positionId:position.positionId}).update(position);
+}
