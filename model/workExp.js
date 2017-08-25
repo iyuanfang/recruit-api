8dd3f1fa678aee62e392bc24f32bb1a81e38fc54
yuanfang <65866693@qq.com>
2017-08-25 15:46:04 +0800

    'model'

--- model/workExp.js
+++ model/workExp.js
@@ -3,13 +3,12 @@ const mongoose=require('../common/mongoose');
 const Schema=mongoose.Schema;
 
 const workExpSchema=new Schema({
-    resumeId:{type: Number},
+    resume:{type:Schema.Types.ObjectId, ref:'resume'},
     company:{type:String},
     position:{type:String},
     beginTime:{type:String},
     endingTime:{type:String},
     description:{type:String},
-
 });
 
 //第三个参数才是指定创建的表名
